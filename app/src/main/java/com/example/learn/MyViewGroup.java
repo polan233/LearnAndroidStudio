0e1c668bff5fe521c4d605d49a5216cac2199194
polan233 <1292437680@qq.com>
2022-09-14 14:49:59 +0800

    新增自己重写的MyLayoutParams内部类,新增customLayoutLP属性,可以在xml里设置right,bottom或者默认的,修改onMeasure和onLayout方法可以适应android:各种margin的样式设置

--- app/src/main/java/com/example/learn/MyViewGroup.java
+++ app/src/main/java/com/example/learn/MyViewGroup.java
@@ -1,6 +1,7 @@
 package com.example.learn;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
@@ -33,7 +34,11 @@ public class MyViewGroup extends ViewGroup {
                 for(int i=0;i<getChildCount();i++){
                     View child=getChildAt(i);
                     measureWidth+=child.getMeasuredWidth();
-                    if(child.getMeasuredHeight()>maxMeasureHeight) maxMeasureHeight=child.getMeasuredHeight();
+
+                    MyLayoutParams lp=(MyLayoutParams) child.getLayoutParams();
+                    measureWidth+=lp.leftMargin+lp.rightMargin;
+
+                    if((child.getMeasuredHeight()+lp.topMargin+lp.topMargin)>maxMeasureHeight) maxMeasureHeight=(child.getMeasuredHeight()+lp.topMargin+lp.topMargin);
                 }
                 setMeasuredDimension(measureWidth,maxMeasureHeight);
             }
@@ -49,11 +54,48 @@ public class MyViewGroup extends ViewGroup {
         int left=0;
         for(int i=0;i<count;i++){
             View child=getChildAt(i);
+
+            MyLayoutParams lp=(MyLayoutParams) child.getLayoutParams();
             int cWidth=child.getMeasuredWidth();
             int cHeight=child.getMeasuredHeight();
 
-            child.layout(left,0,left+cWidth,cHeight);
-            left+=cWidth;
+            if(lp.position==MyLayoutParams.POSITION_RIGHT){
+                child.layout(getWidth()-cWidth-lp.rightMargin,lp.topMargin,getWidth()-lp.rightMargin,lp.topMargin+cHeight);
+            }
+            else if(lp.position==MyLayoutParams.POSITION_BOTTOM){
+                child.layout(left+lp.leftMargin,getHeight()-cHeight-lp.bottomMargin,left+cWidth+lp.leftMargin,getHeight()-lp.bottomMargin);
+            }
+            else{
+                child.layout(left+lp.leftMargin,lp.topMargin,left+cWidth+lp.leftMargin,cHeight+lp.topMargin);
+            }
+            left+=cWidth+lp.leftMargin+lp.rightMargin;
         }
+    }
+
+    @Override
+    public ViewGroup.LayoutParams generateLayoutParams(AttributeSet attrs){
+        return new MyLayoutParams(getContext(),attrs);
+    }
+
+    public static class MyLayoutParams extends MarginLayoutParams {
+        public static int POSITION_RIGHT =1;
+        public static int POSITION_BOTTOM =2;
+
+        public int position = -1;
+
+        public MyLayoutParams(Context c,AttributeSet attrs){
+            super(c,attrs);
+
+            TypedArray a=c.obtainStyledAttributes(attrs,R.styleable.customLayoutLP);
+            position=a.getInt(R.styleable.customLayoutLP_layout_position,position);
+            a.recycle();
+        }
+        public MyLayoutParams(int width,int height){
+            super(width,height);
+        }
+        public MyLayoutParams(ViewGroup.LayoutParams source){
+            super(source);
+        }
+
     }
 }
