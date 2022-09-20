a3394ab3ea98c41acd5e7b72928fe32991889a1a
polan233 <1292437680@qq.com>
2022-09-20 20:56:12 +0800

    自定义Toast的添加,代码还挺多的,其实还可以再看一下

--- app/src/main/java/com/example/learn/ThirdActivity.java
+++ app/src/main/java/com/example/learn/ThirdActivity.java
@@ -3,6 +3,9 @@ package com.example.learn;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Toast;
 
 public class ThirdActivity extends AppCompatActivity {
 
@@ -10,5 +13,31 @@ public class ThirdActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+
+        findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ThirdActivity.this,"这是一个Toast",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        findViewById(R.id.button_custom_top).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                CustomToast.getInstance().showToastCustom(ThirdActivity.this,"自定义Toast在顶部", Gravity.TOP);
+            }
+        });
+        findViewById(R.id.button_custom_mid).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                CustomToast.getInstance().showToastCustom(ThirdActivity.this,"自定义Toast在中间", Gravity.CENTER);
+            }
+        });
+        findViewById(R.id.button_custom_bottom).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                CustomToast.getInstance().showToastCustom(ThirdActivity.this,"自定义Toast在底部", Gravity.BOTTOM);
+            }
+        });
     }
 }
