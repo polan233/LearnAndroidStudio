06b18b341dd33496d51473855fa83453c58c6c78
polan233 <1292437680@qq.com>
2022-09-27 20:17:48 +0800

    Fragment相关练习放去隐式intent那个页面那里了,打印日志看看吧,看了一下transaction的用法 动态添加fragment练习 我还想说什么 到这里了去打e咯

--- app/src/main/java/com/example/learn/FragmentContainer.java
+++ app/src/main/java/com/example/learn/FragmentContainer.java
@@ -1,5 +1,6 @@
 package com.example.learn;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
 
@@ -10,14 +11,18 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link FragmentOne#newInstance} factory method to
+ * Use the {@link FragmentContainer#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class FragmentOne extends Fragment {
-    private  String logTag="FragmentOne";
+public class FragmentContainer extends Fragment {
+    private int fragmentIndex;
+    private String logTag;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -28,43 +33,68 @@ public class FragmentOne extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public FragmentOne() {
+    public FragmentContainer() {
         // Required empty public constructor
     }
-
+    @SuppressLint("ValidFragment")
+    public FragmentContainer(int index){
+        this.fragmentIndex=index;
+        this.logTag="FragmentContainer"+index;
+    }
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentOne.
+     * @return A new instance of fragment FragmentContainer.
      */
     // TODO: Rename and change types and number of parameters
-    public static FragmentOne newInstance(String param1, String param2) {
-        FragmentOne fragment = new FragmentOne();
+    public static FragmentContainer newInstance(String param1, String param2) {
+        FragmentContainer fragment = new FragmentContainer();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i(logTag,"onCreate");
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView=inflater.inflate(R.layout.fragment_container,null);
+        TextView tvContent = (TextView)rootView.findViewById(R.id.textView);
+        switch(fragmentIndex){
+            case(1):
+                tvContent.setText("1!我将以红色形态出击");
+                tvContent.setBackgroundResource(android.R.color.holo_red_light);
+                break;
+            case(2):
+                tvContent.setText("2!我将以橙色色形态出击");
+                tvContent.setBackgroundResource(android.R.color.holo_orange_light);
+                break;
+            case(3):
+                tvContent.setText("3!我将以蓝色形态出击");
+                tvContent.setBackgroundResource(android.R.color.holo_blue_light);
+                break;
+        }
+        Log.i(logTag,"onCreateView");
+        return rootView;
+    }
     @Override
     public void onAttach(Context context){
         super.onAttach(context);
         Log.i(logTag,"onAttach");
-    }
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-        Log.i(logTag,"onCreate");
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState){
-        Log.i(logTag,"onCreateView");
-        return inflater.inflate(R.layout.fragment_one,null);
     }
 
     @Override
@@ -114,5 +144,4 @@ public class FragmentOne extends Fragment {
         super.onDetach();
         Log.i(logTag,"onDetach");
     }
-
 }
--- app/src/main/java/com/example/learn/ThirdActivity.java
+++ app/src/main/java/com/example/learn/ThirdActivity.java
@@ -1,13 +1,17 @@
 package com.example.learn;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Toast;
 
-public class ThirdActivity extends AppCompatActivity {
+public class ThirdActivity extends AppCompatActivity implements View.OnClickListener {
+    private FragmentContainer fragmentOne;
+    private FragmentContainer fragmentTwo;
+    private FragmentContainer fragmentThree;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,5 +43,41 @@ public class ThirdActivity extends AppCompatActivity {
                 CustomToast.getInstance().showToastCustom(ThirdActivity.this,"自定义Toast在底部", Gravity.BOTTOM);
             }
         });
+
+        fragmentOne=new FragmentContainer(1);
+        fragmentTwo=new FragmentContainer(2);
+        fragmentThree=new FragmentContainer(3);
+
+        findViewById(R.id.btn_add).setOnClickListener(this);
+        findViewById(R.id.btn_remove).setOnClickListener(this);
+        findViewById(R.id.btn_replace).setOnClickListener(this);
+        findViewById(R.id.btn_hide).setOnClickListener(this);
+        findViewById(R.id.btn_show).setOnClickListener(this);
+
+    }
+    @Override
+    public void onClick(View v){
+        FragmentTransaction transaction= getSupportFragmentManager().beginTransaction();
+        switch(v.getId()){
+            case(R.id.btn_add):
+                transaction.add(R.id.fl_container,fragmentOne);
+                transaction.add(R.id.fl_container,fragmentTwo);
+                transaction.addToBackStack(null);
+                break;
+            case(R.id.btn_remove):
+                transaction.remove(fragmentTwo);
+                break;
+            case(R.id.btn_replace):
+                transaction.replace(R.id.fl_container,fragmentThree);
+                transaction.addToBackStack(null);
+                break;
+            case(R.id.btn_hide):
+                transaction.hide(fragmentThree);
+                break;
+            case(R.id.btn_show):
+                transaction.show(fragmentThree);
+                break;
+        }
+        transaction.commit();
     }
 }
